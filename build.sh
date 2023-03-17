54f81780b09a8a8e0d317218f997eeee21735822
zehara <107530847+zehara19@users.noreply.github.com>
2023-03-17 09:27:45 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cat <<EOT >Dockerfile
+FROM node:16-alpine as build
+WORKDIR /zehbuildapp
+COPY package*.json /zehbuildapp/
+RUN npm install
+COPY . .
+RUN npm run build
+FROM nginx:latest
+COPY --from=build /zehbuildapp/build/ /usr/share/nginx/html/
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
+EOT
+
+echo "Completed creating docker file. Below are its contents"
+
+cat Dockerfile
+
+
+echo "Now we will create an image using this docker file with when function using jenkins "
+
+docker build -t zehara19/capstone-public-dev:${BUILD_NUMBER} .
+
+
+
+
+
